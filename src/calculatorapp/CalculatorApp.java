2b62f83482ffba9fbb33952f1bdbcebdff48dadc
Georgios Orfanidis <OrfaG4@users.noreply.github.com>
2015-11-02 01:14:14 +0200

    Create CalculatorApp.java

---
+++ src/calculatorapp/CalculatorApp.java
@@ -0,0 +1,13 @@
+package calculatorapp;
+
+public class CalculatorApp {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        CalculatorForm asd = new CalculatorForm();
+       asd.setVisible(true);
+    }
+    
+}
